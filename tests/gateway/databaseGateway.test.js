b08e59a5bc40d8dbaa619975912fb0c58c8740bd
b-dalton <ben.dalton@madetech.com>
2022-01-13 13:13:59 -0400

    test retrieveRecipes from database gateway

---
+++ tests/gateway/databaseGateway.test.js
@@ -0,0 +1,38 @@
+import DatabaseGateway from '../../src/gateway/databaseGateway.js';
+
+describe('Database gateway', () => {
+  it('retrieve recipes method returns all recipes', () => {
+    const databaseGateway = new DatabaseGateway();
+
+    const recipes = [
+      {
+        title: 'Recipe 1',
+        ingredients: ['spinach'],
+        prepTime: '5 mins',
+        cookTime: '30 mins',
+        serves: 3,
+        instructions: 'cook spinach',
+        author: 'Ben',
+        timestamp: '21-12-16 09:17:03',
+      },
+      {
+        title: 'Another Recipe',
+        ingredients: ['tomato'],
+        prepTime: '10 mins',
+        cookTime: '20 mins',
+        serves: 1,
+        instructions: 'cook tomato',
+        author: 'Ben',
+        timestamp: '22-01-11 12:00:17',
+      },
+    ];
+
+    const retrieveRecipesMethodSpy = jest.spyOn(
+      databaseGateway,
+      'retrieveRecipes'
+    );
+    retrieveRecipesMethodSpy.mockReturnValue(recipes);
+
+    expect(databaseGateway.retrieveRecipes()).toBe(recipes);
+  });
+});
