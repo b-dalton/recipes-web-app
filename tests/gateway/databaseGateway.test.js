16eb029605b8c6b46f6d536f8d11f5e3ee4271d7
b-dalton <ben.dalton@madetech.com>
2022-01-29 19:10:48 +0000

    Set up test recipes database
    Creates a test database and populates with some recipes before
    running tests and drops the database after all tests have run

--- tests/gateway/databaseGateway.test.js
+++ tests/gateway/databaseGateway.test.js
@@ -1,8 +1,55 @@
 import DatabaseGateway from '../../src/gateway/databaseGateway.js';
+const { Client } = require('pg');
+const env = require('node-env-file');
+env('./.env');
+
+const credentials = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+const client = new Client(credentials);
+
+beforeAll(async () => {
+  await client.connect();
+  await client.query(
+    `CREATE TABLE recipes (
+        title varchar(255) NOT NULL, 
+        ingredients text[] NOT NULL, 
+        prep_time VARCHAR(50) NOT NULL, 
+        cook_time VARCHAR (50) NOT NULL, 
+        serves INT NOT NULL, 
+        instructions TEXT NOT NULL, 
+        author VARCHAR (50) NOT NULL, 
+        updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+      );`
+  );
+  await client.query(
+    `INSERT INTO 
+        recipes (title, ingredients, prep_time, cook_time, serves, instructions, author, updated) 
+    VALUES 
+        ('Spinach recipe', '{\"spinach\"}', '5 mins', '10 mins', 3, 'cook spinach', 'Ben', '2022-01-29T18:30:13.542Z'),
+        ('Tomato Soup', '{\"tomato puree\", \"coconut milk\", \"water\"}', '15 mins', '30 mins', 2, 'Mix tomato puree, coconut milk and water and bring to boil. Season with salt and pepper', 'Ben', '2022-01-12T13:33:03.542Z'),
+        ('Sweet Potato Mash', '{\"2 sweet potatoes\", \"50ml milk\", \"2 tbsp butter\"}', '10 mins', '30 mins', 4, 'Boil sweet potatoes until soft. Mash sweet potatoes, with butter and milk', 'Ben', '2021-12-16T08:12:08.542Z');`
+  );
+});
+
+afterAll(async () => {
+  await client.query('DROP TABLE IF EXISTS recipes;');
+  await client.end();
+});
 
 describe('Database gateway', () => {
   it('retrieve recipes method returns all recipes', () => {
     const databaseGateway = new DatabaseGateway();
+
+    const retrieveRecipesMethodSpy = jest.spyOn(
+      databaseGateway,
+      'retrieveRecipes'
+    );
 
     const recipes = [
       {
@@ -26,14 +73,20 @@ describe('Database gateway', () => {
         timestamp: '22-01-11 12:00:17',
       },
     ];
-
-    const retrieveRecipesMethodSpy = jest.spyOn(
-      databaseGateway,
-      'retrieveRecipes'
-    );
     retrieveRecipesMethodSpy.mockReturnValue(recipes);
 
     expect(databaseGateway.retrieveRecipes()).toBe(recipes);
     expect(retrieveRecipesMethodSpy).toHaveBeenCalled();
+  });
+
+  it('Receives recipes from database', async () => {
+    const allRecords = await client.query('SELECT * FROM recipes');
+    console.log('all recipes:', allRecords.rows);
+
+    expect(allRecords.rows[0].author).toBe('Ben');
+    expect(allRecords.rows[0].title).toBe('Spinach recipe');
+    expect(allRecords.rows[1].title).toBe('Tomato Soup');
+    expect(allRecords.rows[1].prep_time).toBe('15 mins');
+    expect(allRecords.rows[2].title).toBe('Sweet Potato Mash');
   });
 });
